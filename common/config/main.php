bf84454c68405f4fa75f91f8fa0de79d5f4f1446
LanCodeD <lancode17@gmail.com>
2025-06-04 17:58:33 -0600

    Version del libro capitulo Nueve Yii2 - No se usa Kartik

--- common/config/main.php
+++ common/config/main.php
@@ -5,9 +5,48 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
+    'modules' => [
+        'social' => [
+            // la clase del módulo
+            'class' => 'kartik\social\Module',
+
+            // la configuración global para el widget disqus
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // conf por defecto
+            ],
+
+            // la configuración global para el widget de los plugins de facebook
+            'facebook' => [
+                'appId' => '2888418048022004',
+                'secret' => 'cc6f461a292e2614c6a6f60ef54e7ae2',
+            ],
+
+            // la configuración social para el widget del plugin social de google
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+
+            // la configuración global para el widget del plugin de google analytic
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN',
+            ],
+
+            // la configuración global para el widget del plugin de twitter
+            'twitter' => [
+                'screenName' => 'TWITTER_SCREEN_NAME'
+            ],
+        ],
+        // sus otros módulos
+    ],
+
     'components' => [
         'cache' => [
-            'class' => \yii\caching\FileCache::class,
+            'class' => 'yii\caching\FileCache',
         ],
+
     ],
 ];
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,52 +1,141 @@
 <?php
-
 /** @var yii\web\View $this */
+use yii\helpers\Html;
 
-$this->title = 'My Yii Application';
+$this->title = 'Yii 2 Build';
 ?>
+
+<!-- Facebook SDK (solo una vez por página) -->
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous"
+    src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v17.0"
+    nonce="abc123"></script>
+
 <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+
+    <div class="p-5 mb-4 bg-light rounded-3 text-center">
+        <?php if (Yii::$app->user->isGuest): ?>
+            <?= Html::a('Comience Hoy', ['site/signup'], ['class' => 'btn btn-lg btn-success']) ?>
+        <?php endif; ?>
+
+        <h1>Yii 2 Build <i class="fa fa-plug"></i></h1>
+        <p class="lead">Use esta plantilla de Yii 2 para comenzar Proyectos.</p>
+
+        <!-- Botón Like de Facebook -->
+        <div class="fb-like" data-href="http://www.yii2build.com" data-width=""
+            data-layout="standard" data-action="like" data-size="small"
+            data-share="true"></div>
+    </div>
+
+    <!-- Collapse estilo Bootstrap 5 -->
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne" aria-expanded="true"
+                    aria-controls="collapseOne">
+                    Características Principales
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show"
+                aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="fb-share-button" data-href="http://www.yii2build.com"
+                        data-layout="button_count"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#collapseTwo"
+                    aria-expanded="false" aria-controls="collapseTwo">
+                    Recursos Principales
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse"
+                aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <div class="fb-share-button" data-href="http://www.yii2build.com"
+                        data-layout="button_count"></div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="body-content">
+    <!-- Modal trigger -->
+    <button type="button" class="btn btn-primary mt-4" data-bs-toggle="modal" data-bs-target="#comentariosModal">
+        Comentarios
+    </button>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <!-- Modal Bootstrap 5 -->
+    <div class="modal fade" id="comentariosModal" tabindex="-1" aria-labelledby="comentariosModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="comentariosModalLabel">Últimos Comentarios</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="fb-comments" data-href="http://www.yii2build.com"
+                        data-width="100%" data-numposts="5"></div>
+                </div>
             </div>
         </div>
+    </div>
 
+    <!-- Alert de Bootstrap 5 -->
+    <div class="alert alert-info mt-4" role="alert">
+        Lance su proyecto como un cohete...
+    </div>
+
+    <div class="body-content mt-4">
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Gratis</h2>
+                <p>
+                    <?php
+                    if (!Yii::$app->user->isGuest) {
+                        echo Yii::$app->user->identity->username . ' está haciendo cosas geniales. ';
+                    }
+                    ?>
+                    Partiendo de esta plantilla de código abierto y gratuita de Yii 2 ahorrará mucho tiempo.
+                </p>
+                <p>
+                    <a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc-2.0/guide-index.html">
+                        Documentación de Yii &raquo;
+                    </a>
+                </p>
+                <div class="fb-like" data-href="http://www.yii2build.com" data-width=""
+                    data-layout="standard" data-action="like" data-size="small"
+                    data-share="false"></div>
+            </div>
+            <div class="col-lg-4">
+                <h2>Ventajas</h2>
+                <p> La facilidad de uso es una enorme ventaja.  Hemos simplificado el RBAC y le hemos
+                    otorgado tipos de usuario Gratuito/Pago de manera predeterminada.  Los plugins sociales
+                    son tan sencillos y rápidos de instalar, ¡que los amará!</p>
+                <p>
+                    <a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">
+                        Foro de Yii &raquo;
+                    </a>
+                </p>
+                <div class="fb-comments" data-href="http://www.yii2build.com"
+                    data-width="100%" data-numposts="2"></div>
+            </div>
+            <div class="col-lg-4">
+                <h2>¡Codifique Rápidamente, Codifique Correctamente!</h2>
+                <p> Libere el poder del asombroso framework Yii 2 con esta plantilla mejorada.
+                    Con base en la plantilla avanzada de Yii 2, obtiene una implementación de frontend y
+                    backend completa que presenta una IU rica para la administración del backend.</p>
+                <p>
+                    <a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">
+                        Extensiones de Yii &raquo;
+                    </a>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
